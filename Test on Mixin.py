6df467d58a6c785edbc6b96dc82df2af6c5433a4
Damnsure <bing900713@hotmail.com>
2018-05-28 13:14:08 +0800

    no message

--- Test on Mixin.py
+++ Test on Mixin.py
@@ -1,11 +1,27 @@
 __author__ = 'nickyuan'
 
-class SimpleItemContainer(object):
-    def __init__(self, id, item_container):
-        self.id = id
-        self.data = {}
-        for item in item_container:
-            self.data[item.id] = item
+# class SimpleItemContainer(object):
+#     def __init__(self, id, item_container):
+#         self.id = id
+#         self.data = {}
+#         for item in item_container:
+#             self.data[item.id] = item
+#
+# containerA = SimpleItemContainer(2,['a','b','c'])
+# print(containerA)
 
-containerA = SimpleItemContainer(2,['a','b','c'])
-print(containerA)
+from delorean import Delorean
+
+
+d = Delorean()
+d = d.shift("Asia/Shanghai")
+
+
+from requests import request
+
+print(d)
+print(d.datetime, d.date)
+
+import wget
+
+print(wget.download("http://music.163.com/#/song?id=27678693"))
